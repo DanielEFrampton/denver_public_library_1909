6c381abc3600da5925dbb35d4ed9b547143eec5b
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-12-09 21:00:26 -0700

    Add checkout, return, and checked_out_books methods to library class

--- lib/library.rb
+++ lib/library.rb
@@ -4,18 +4,30 @@ class Library
   def initialize(name)
     @name = name
     @authors = []
-  end
-
-  def books
-    @authors.flat_map { |author| author.books }
+    @books = []
   end
 
   def add_author(author)
     @authors << author
+    @books += author.books
   end
 
   def publication_time_frame_for(author)
     { start: author.earliest_book.publication_year,
     end: author.latest_book.publication_year}
+  end
+
+  def checkout(book)
+    @books.include?(book) && !book.checked_out ? book.checkout : false
+  end
+
+  def return(book)
+    book.return
+  end
+
+  def checked_out_books
+    @books.find_all do |book|
+      book.checked_out
+    end
   end
 end
